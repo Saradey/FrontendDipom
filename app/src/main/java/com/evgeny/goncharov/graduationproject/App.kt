7d700d9100dcda4059dedae0b3de8949e062f7b8
Evgeny Goncharov <jtgn@yandex.ru>
2019-03-18 00:40:49 +0300

    Initial commit

---
+++ app/src/main/java/com/evgeny/goncharov/graduationproject/App.kt
@@ -0,0 +1,15 @@
+package com.evgeny.goncharov.graduationproject
+
+import android.app.Application
+
+class App : Application(){
+
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/evgeny/goncharov/graduationproject/ui/activity/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.evgeny.goncharov.graduationproject.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.evgeny.goncharov.graduationproject.R
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_registration)
+
+        
+    }
+
+
+
+}
