90fe9b067879bddab7712506dfe4e4c831662c71
Evgeny Goncharov <jtgn@yandex.ru>
2019-06-24 13:16:13 +0300

    first and last commit

--- app/src/main/java/com/evgeny/goncharov/graduationproject/consts/ApiMethods.kt
+++ app/src/main/java/com/evgeny/goncharov/graduationproject/consts/ApiMethods.kt
@@ -1,7 +1,7 @@
 package com.evgeny.goncharov.graduationproject.consts
 
 
-const val BASE_URL = "https://172.20.10.2:8443/"
+const val BASE_URL = "https://46.173.218.84:8443/"
 
 //user
 const val USER_AUTHENTICATION = "user/authentication"
@@ -21,4 +21,3 @@ const val ARTICLE_GET_LAST_ARTICLE = "article/getLastArticle"
 const val ARTICLE_GET_OFFSET = "article/getArticleOffset"
 
 const val ARTICLE_GET_FOR_USER = "article/getForUser"
-
